9a2ab598627bb037a5b4fcc86894279622605252
Ana Kloss <anak492@gmail.com>
2022-08-04 12:15:54 -0300

    error css

--- PagWeb/webApp/views.py
+++ PagWeb/webApp/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(req):
-    return render(req, 'webApp/home.html')
+    return render(req, 'webApp/index.html')
 
 def service(req):
     return HttpResponse("Service")
