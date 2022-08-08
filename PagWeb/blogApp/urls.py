ed81837041835b3d11cfa4295d48358e191dba7f
Ana Kloss <anak492@gmail.com>
2022-08-08 11:40:36 -0300

    modif blogAppp

--- PagWeb/blogApp/urls.py
+++ PagWeb/blogApp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.blog_home, name='Blog home'),
     path('post/', views.blog_post, name='Blog post'),
+    path('category/<int:category_id>/', views.category, name="Categoria")  # <int:category_id>
 ]
--- PagWeb/blogApp/views.py
+++ PagWeb/blogApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Category
 
 
 def blog_home(req):
@@ -8,3 +8,8 @@ def blog_home(req):
 
 def blog_post(req):
     return render(req, 'blogApp/blog-post.html')
+
+def category(req, category_id):
+    category = Category.objects.get(id=category_id)
+    posts = Post.objects.filter(categories=category)
+    return render(req, 'blogApp/categories.html', {'category': category, 'posts': posts})
