8c0a38ec65a2e1a8fa0520a6c023ad7f3054a8ea
Ana Kloss <anak492@gmail.com>
2022-08-16 09:47:13 -0300

    modif send_mail

--- PagWeb/pedidosApp/views.py
+++ PagWeb/pedidosApp/views.py
@@ -5,6 +5,7 @@ from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.core.mail import send_mail
 from carroApp.carro import Cart
+from django.conf import settings
 from .models import Order, OrderLine
 
 
@@ -23,8 +24,11 @@ def procesar_pedido(request):
     # Guarda en la tabla de la BD
     OrderLine.objects.bulk_create(lineas_pedido)
 
-    enviar_mail(pedido=pedido, lineas_pedido=lineas_pedido, username=request.user.username, email=request.user.email)
-    messages.success(request, "El pedido se ha creado correctamente")
+    try:
+        enviar_mail(pedido=pedido, lineas_pedido=lineas_pedido, username=request.user.username, email=request.user.email)
+        messages.success(request, "El pedido se ha creado correctamente")
+    except:
+        messages.error(request, "Hubo un error al enviar pedido")
     return redirect('Market')
 
 
@@ -33,9 +37,11 @@ def enviar_mail(**kwargs):
     mensaje=render_to_string("emails/pedido.html", {
         "pedido": kwargs.get("pedido"),
         "lineas_pedido": kwargs.get("lineas_pedido"),
-        "username": kwargs.get("username")
+        "username": kwargs.get("username"),
+        "email": kwargs.get("email")
     })
     mensaje_txt=strip_tags(mensaje)
-    from_email="anak492@gmail.com"
+    # from_email="edna.haley@ethereal.email"
+    from_email = settings.EMAIL_HOST_USER
     to=kwargs.get("email")
     send_mail(asunto, mensaje_txt, from_email, [to], html_message=mensaje)
